0040327b7f2a8c4463e3b3f96c81009650a7cf3d
Louis Griffiths <louisg0709@gmail.com>
2021-11-19 16:56:48 +0000

    first commit

---
+++ petsclasses.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 16:12:38 2021
+Python Pets classes
+@author: louis
+"""
+
+#A simple text based python game where you look after pets.
+
+#Main game class
+class game:
+    def __init__(self):
+        self.status=1
+        return self
+#Main class that deals with your pets
+#pets will have a energy,happiness,care and clean value.
+class pet:
+    def __init__(self):
+        self.happy = 5
+        return self
+    
+    def startpet(self):
+     
+        return self
+        
---
+++ petsmain.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 16:10:34 2021
+Python Pets 
+@author: louis
+"""
+#A simple text based python game where you look after pets.
+
+
+#main loop
+petname = input('What would you like to name your pet? ')
+print('Your pet is called',petname)
