a025206d9dd303e5bade65221309940579b100ac
Louis Griffiths <louisg0709@gmail.com>
2021-11-21 13:05:00 +0000

    Added a basic setup class

--- petsclasses.py
+++ petsclasses.py
@@ -5,8 +5,6 @@ Python Pets classes
 @author: louis
 """
 
-#A simple text based python game where you look after pets.
-
 #Main game class
 class game:
     def __init__(self):
@@ -16,10 +14,16 @@ class game:
 #pets will have a energy,happiness,care and clean value.
 class pet:
     def __init__(self):
-        self.happy = 5
-        return self
+        self.name="Bob"
+        
     
     def startpet(self):
-     
+        pet1 = input('What would you like to name your pet? ')
+        print('Your pet is called',pet1)
+        #self.name = pet1 This line causes error int has no attribute name
+        pet1food = 5
+        pet1clean = 5
+        pet1fun = 5
+        pet1love = 5
         return self
         
--- petsmain.py
+++ petsmain.py
@@ -4,9 +4,12 @@ Created on Fri Nov 19 16:10:34 2021
 Python Pets 
 @author: louis
 """
-#A simple text based python game where you look after pets.
-
+#Importing classes
+from petsclasses import *
 
 #main loop
-petname = input('What would you like to name your pet? ')
-print('Your pet is called',petname)
+#petname = input('What would you like to name your pet? ')
+#print('Your pet is called',petname)
+
+pet1=pet()
+pet1.startpet()
