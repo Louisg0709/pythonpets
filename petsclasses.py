e8decff471b38cf90fe97d5a68598eca72ac4de5
Louis Griffiths <louisg0709@gmail.com>
2021-11-24 20:09:19 +0000

    Added a class taht detects your commands

--- petsclasses.py
+++ petsclasses.py
@@ -11,19 +11,51 @@ class game:
         self.status=1
         return self
 #Main class that deals with your pets
-#pets will have a energy,happiness,care and clean value.
 class pet:
     def __init__(self):
         self.name="Bob"
+        self.food=0
+        self.clean=0
+        self.fun=0
+        self.love=0
         
-    
     def startpet(self):
-        pet1 = input('What would you like to name your pet? ')
-        print('Your pet is called',pet1)
+        pet = input('What would you like to name your pet?\n')
+        print('Your pet is called',pet)
         #self.name = pet1 This line causes error int has no attribute name
-        pet1food = 5
-        pet1clean = 5
-        pet1fun = 5
-        pet1love = 5
+        self.food = 5
+        self.clean = 5
+        self.fun = 5
+        self.love = 5
         return self
+    
+    #starts an new pet which is the same as copypet
+    def copypet(self,copypet):
+        pet = input('What would you like to name your pet?\n')
+        print('Your pet is called',pet)
+        #self.name = pet1 This line causes error int has no attribute name
+        self.food = copypet.food
+        self.clean = copypet.clean
+        self.fun = copypet.fun
+        self.love = copypet.love
+        return self
+
+#Class that deals with commands
+class command:
+    def __init__(self):
+        self.command="help" 
+        
+    #get command from user            
+    def request(self):
+        #print('command requested')
+        self.command=input()
+        return self.command
+        
+    #trigger an action        
+    def check(self,command):
+        event=0
+        if self.command == 'help':
+            help=1
+            print('help')
+        return event
         
--- petsmain.py
+++ petsmain.py
@@ -4,12 +4,25 @@ Created on Fri Nov 19 16:10:34 2021
 Python Pets 
 @author: louis
 """
+#variables
+game = 1
+
 #Importing classes
 from petsclasses import *
 
-#main loop
-#petname = input('What would you like to name your pet? ')
-#print('Your pet is called',petname)
+#Functions
 
+game=command()
+requestcommand = game.request()
+checkcommand = game.check(requestcommand)
+
+#main loop
 pet1=pet()
 pet1.startpet()
+
+#This code will later be used to create a new pet
+#pet2=pet()          
+#pet2.copypet(pet1)
+
+game.request()
+game.check(requestcommand)
